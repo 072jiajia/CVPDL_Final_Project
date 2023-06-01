3ffa33c33f15429f7226c2bc8119fe8ce0b53a28
lijenchang <j89070483@gmail.com>
2023-06-02 00:43:45 +0800

    Add CoCoOp v2: replace addition with concat

--- prompter.py
+++ prompter.py
@@ -290,6 +290,97 @@ class CoCoOpPromptLearner(nn.Module):
         return prompts
 
 
+class CoCoOpPromptLearnerV2(nn.Module):
+    """
+    Ref: https://github.com/KaiyangZhou/CoOp/blob/main/trainers/cocoop.py
+    """
+    def __init__(self, classnames, clip_model, cfg=DEFAULT_CFG):
+        super().__init__()
+        n_cls = len(classnames)
+        n_ctx = cfg.N_CTX
+        dtype = clip_model.dtype
+        ctx_dim = clip_model.ln_final.weight.shape[0]
+        vis_dim = clip_model.visual.output_dim
+
+        # random initialization
+        ctx_vectors = torch.empty(n_ctx - 1, ctx_dim, dtype=dtype)
+        nn.init.normal_(ctx_vectors, std=0.02)
+        prompt_prefix = " ".join(["X"] * n_ctx)
+
+        print(f'Initial context: "{prompt_prefix}"')
+        print(f"Number of context words (tokens): {n_ctx}")
+
+        ##### to be optimized
+        self.ctx = nn.Parameter(ctx_vectors)
+
+        self.meta_net = nn.Sequential(OrderedDict([
+            ("linear1", nn.Linear(vis_dim, vis_dim // 16)),
+            ("relu", nn.ReLU(inplace=True)),
+            ("linear2", nn.Linear(vis_dim // 16, ctx_dim))
+        ]))
+        #####
+
+        classnames = [name.replace("_", " ") for name in classnames]
+        name_lens = [len(_tokenizer.encode(name)) for name in classnames]
+        prompts = [prompt_prefix + " " + name + "." for name in classnames]
+
+        tokenized_prompts = torch.cat([clip.tokenize(p) for p in prompts])  # (n_cls, n_tkn)
+        with torch.no_grad():
+            embedding = clip_model.token_embedding(tokenized_prompts).type(dtype)
+
+        # These token vectors will be saved when in save_model(),
+        # but they should be ignored in load_model() as we want to use
+        # those computed using the current class names
+        self.register_buffer("token_prefix", embedding[:, :1, :])  # SOS
+        self.register_buffer("token_suffix", embedding[:, 1 + n_ctx:, :])  # CLS, EOS
+
+        self.n_cls = n_cls
+        self.n_ctx = n_ctx
+        self.tokenized_prompts = tokenized_prompts  # torch.Tensor
+        self.name_lens = name_lens
+    
+    def construct_prompts(self, ctx, prefix, suffix, label=None):
+        # dim0 is either batch_size (during training) or n_cls (during testing)
+        # ctx: context tokens, with shape of (dim0, n_ctx, ctx_dim)
+        # prefix: the sos token, with shape of (n_cls, 1, ctx_dim)
+        # suffix: remaining tokens, with shape of (n_cls, *, ctx_dim)
+
+        if label is not None:
+            prefix = prefix[label]
+            suffix = suffix[label]
+
+        prompts = torch.cat(
+            [
+                prefix,  # (dim0, 1, dim)
+                ctx,     # (dim0, n_ctx, dim)
+                suffix,  # (dim0, *, dim)
+            ],
+            dim=1,
+        )
+
+        return prompts
+
+    def forward(self, im_features):
+        prefix = self.token_prefix
+        suffix = self.token_suffix
+        ctx = self.ctx                                # (n_ctx - 1, ctx_dim)
+        bias = self.meta_net(im_features)             # (batch, ctx_dim)
+        bias = bias.unsqueeze(1)                      # (batch, 1, ctx_dim)
+        ctx = ctx.unsqueeze(0)                        # (1, n_ctx - 1, ctx_dim)
+        ctx = ctx.expand(bias.shape[0], -1, -1)       # (batch, n_ctx - 1, ctx_dim)
+        ctx_shifted = torch.cat((ctx, bias), dim=1)   # (batch, n_ctx, ctx_dim)
+        
+        # Use instance-conditioned context tokens for all classes
+        prompts = []
+        for ctx_shifted_i in ctx_shifted:
+            ctx_i = ctx_shifted_i.unsqueeze(0).expand(self.n_cls, -1, -1)
+            pts_i = self.construct_prompts(ctx_i, prefix, suffix)  # (n_cls, n_tkn, ctx_dim)
+            prompts.append(pts_i)
+        prompts = torch.stack(prompts)
+        
+        return prompts
+
+
 class CLIPPrompter(nn.Module):
     def __init__(self, n_emb_per_class, classnames, clip_model, cfg=DEFAULT_CFG, trainer="coop"):
         super().__init__()
@@ -299,6 +390,10 @@ class CLIPPrompter(nn.Module):
             self.prompt_learner = PromptLearner(classnames, clip_model, cfg)
         elif trainer == "cocoop":
             self.prompt_learner = CoCoOpPromptLearner(classnames, clip_model, cfg)
+            self.image_encoder = clip_model.visual
+            self.dtype = clip_model.dtype
+        elif trainer == "cocoopv2":
+            self.prompt_learner = CoCoOpPromptLearnerV2(classnames, clip_model, cfg)
             self.image_encoder = clip_model.visual
             self.dtype = clip_model.dtype
         else:
